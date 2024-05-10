f921a51e97b7bfea9cffdfaf906318e32404e81c
MBandus <maryna_bandus@qollabe.com>
2024-05-10 13:12:35 +0300

    responsive changes

--- slider/js/main.js
+++ slider/js/main.js
@@ -10,6 +10,27 @@ const swiper = new Swiper('.swiper', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
 });
 
 document.addEventListener("DOMContentLoaded", function() {
